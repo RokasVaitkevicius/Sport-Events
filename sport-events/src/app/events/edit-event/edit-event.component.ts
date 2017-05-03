aa3c49c9eaa764662b4e8bc3bb7b1bbe5880f810
Rokas Vaitkevičius <r.vaitkevicius@loginvsi.com>
2017-05-03 23:06:23 +0300

    - Some work on event details component - Almost completed event edit component

--- sport-events/src/app/events/edit-event/edit-event.component.ts
+++ sport-events/src/app/events/edit-event/edit-event.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {IEvent} from '../shared/event.model';
+import {ActivatedRoute} from '@angular/router';
+import {EventService} from '../shared/event.service';
+import {FormControl, FormGroup} from '@angular/forms';
 
 @Component({
   selector: 'edit-event',
@@ -6,10 +10,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-event.component.css']
 })
 export class EditEventComponent implements OnInit {
+  event: IEvent;
+  eventForm: FormGroup;
 
-  constructor() { }
+  constructor(private eventService: EventService,
+              private route:ActivatedRoute) { }
 
   ngOnInit() {
+    this.event = this.eventService.getEvent
+    (+this.route.snapshot.params['id']);
+    this.eventForm = new FormGroup({
+      author: new FormControl(this.event.author),
+      name: new FormControl(this.event.name),
+      sportType: new FormControl(this.event.sportType),
+      date: new FormControl(this.event.date),
+      timeFrom: new FormControl(this.event.timeFrom),
+      timeTill: new FormControl(this.event.timeTill),
+      price: new FormControl(this.event.price),
+      phoneNumber: new FormControl(this.event.phoneNumber),
+      address: new FormControl(this.event.location.address),
+      city: new FormControl(this.event.location.city),
+      country: new FormControl(this.event.location.country),
+      description: new FormControl(this.event.description),
+      facebookEventUrl: new FormControl(this.event.facebookEventUrl),
+      imageUrl: new FormControl(this.event.imageUrl)
+    });
+
   }
 
 }
